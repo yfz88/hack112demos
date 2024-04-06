955ca432dc29314854565ea8a5777391cf67e92b
ShawneeBoy <shawn4jworld@gmail.com>
2024-04-06 15:19:08 -0400

    Made flask demo easier to run

--- flask_demo/demo.py
+++ flask_demo/demo.py
@@ -4,6 +4,8 @@ Flask can be used to quickly develop and host web pages.
 This code just loads a simple HTML page, located at templates/sample.html.
 To run, type "py -m flask --app demo run" into your terminal.
 
+You can also just run this file directly with "python demo.py"
+provided you have the if __name__ == '__main__': block at the bottom.
 '''
 
 from flask import Flask, render_template
@@ -13,3 +15,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('sample.html')
+
+if __name__ == '__main__':
+  app.run(port=7777, debug=True)
